2465f18ada5871b0e9fed5bd5b8553898211d730
dejvokep <davidcubesvkdev@gmail.com>
2022-12-04 11:56:46 +0100

    updated icons

--- app/pages/green/index.js
+++ app/pages/green/index.js
@@ -1,4 +1,5 @@
 import styles from "../../styles/Find.module.css";
+import styles2 from "../../styles/MainMenu.module.css";
 import LineButton from "../../components/LineButton";
 import MainMenuForeign from "../../components/MainMenuForeign";
 import Image from "next/image";
@@ -11,24 +12,24 @@ export default function Page() {
         </div>
 
         <div className={styles.center}>
-            <div>
-                <div>
-                    <Link href={"/green/ebike"}><Image src={"/menu_btns/btnBike.png"} alt={"Image"} height={60} width={175} /></Link>
+            <div className={styles2.buttonBox}>
+                <div className={styles2.buttonRow}>
+                    <div className={styles2.smallButton}>
+                        <Link href={"/green/ebike"}><Image src={"/menu_btns/btnBike.png"} alt={"Image"} height={60} width={175} /></Link>
+                    </div>
+                    <div className={styles2.smallButton}>
+                        <Link href={"/green/emoped"}><Image src={"/menu_btns/btnMoped.png"} alt={"Image"} height={60} width={175} /></Link>
+                    </div>
                 </div>
-
-                <div>
-                    <Link href={"/green/emoped"}><Image src={"/menu_btns/btnMoped.png"} alt={"Image"} height={60} width={175} /></Link>
-                </div>
-
-                <div>
-                    <Link href={"/green/escooter"}><Image src={"/menu_btns/btnScooter.png"} alt={"Image"} height={60} width={175} /></Link>
-                </div>
-
-                <div>
-                    <Link href={"/green/mhd"}><Image src={"/greenslides/mhd_btn.png"} alt={"Image"} height={60} width={175} /></Link>
+                <div className={styles2.buttonRow}>
+                    <div className={styles2.smallButton}>
+                        <Link href={"/green/escooter"}><Image src={"/menu_btns/btnScooter.png"} alt={"Image"} height={60} width={175} /></Link>
+                    </div>
+                    <div className={styles2.smallButton}>
+                        <Link href={"/green/mhd"}><Image src={"/greenslides/mhd_btn.png"} alt={"Image"} height={60} width={175} /></Link>
+                    </div>
                 </div>
             </div>
-
         </div>
 
         <MainMenuForeign />
